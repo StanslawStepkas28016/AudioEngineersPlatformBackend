2a90859e43b399e7963cd29a7faa485399f54ee3
Stanisław Stepka <stanislawstepka@Macbook-Pro-Stanisaw.local>
2025-05-05 21:17:37 +0200

    Refactor for Clean Architecture

---
+++ AudioEngineersPlatformBackend.Application/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿namespace AudioEngineersPlatformBackend.Application;
+
+using Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
+    {
+        // services.AddScoped<IRecruitmentsService, RecruitmentsService>();
+        return services;
+    }
+}
