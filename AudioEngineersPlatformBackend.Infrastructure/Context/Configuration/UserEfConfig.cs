2d63686527e471cc1d47b00c65de35ccbaf9c2c9
Stanisław Stepka <s28016@pjwstk.edu.pl>
2025-07-02 16:53:17 +0200

    Add index for checkAuth for performance increase

--- AudioEngineersPlatformBackend.Infrastructure/Context/Configuration/UserEfConfig.cs
+++ AudioEngineersPlatformBackend.Infrastructure/Context/Configuration/UserEfConfig.cs
@@ -4,53 +4,65 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace AudioEngineersPlatformBackend.Infrastructure.Context.Configuration;
 
-    public class UserEfConfig : IEntityTypeConfiguration<User>
+public class UserEfConfig : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
     {
-        public void Configure(EntityTypeBuilder<User> builder)
-        {
-            builder
-                .HasKey(u => u.IdUser)
-                .HasName("PK_User");
+        builder
+            .HasKey(u => u.IdUser)
+            .HasName("PK_User");
 
-            builder
-                .Property(u => u.IdUser)
-                .IsRequired();
+        builder
+            .Property(u => u.IdUser)
+            .IsRequired();
 
-            builder
-                .Property(u => u.FirstName)
-                .IsRequired();
+        builder
+            .Property(u => u.FirstName)
+            .IsRequired();
 
-            builder
-                .Property(u => u.LastName)
-                .IsRequired();
+        builder
+            .Property(u => u.LastName)
+            .IsRequired();
 
-            builder
-                .Property(u => u.Email)
-                .IsRequired();
+        builder
+            .Property(u => u.Email)
+            .IsRequired();
 
-            builder
-                .Property(u => u.PhoneNumber)
-                .IsRequired();
+        builder
+            .Property(u => u.PhoneNumber)
+            .IsRequired();
 
-            builder
-                .Property(u => u.Password)
-                .IsRequired();
+        builder
+            .Property(u => u.Password)
+            .IsRequired();
 
-            builder
-                .HasOne(u => u.Role)
-                .WithMany(u => u.Users)
-                .HasForeignKey(u => u.IdRole)
-                .HasConstraintName("FK_User_Role")
-                .OnDelete(DeleteBehavior.Restrict);
+        builder
+            .HasOne(u => u.Role)
+            .WithMany(u => u.Users)
+            .HasForeignKey(u => u.IdRole)
+            .HasConstraintName("FK_User_Role")
+            .OnDelete(DeleteBehavior.Restrict);
 
-            builder
-                .HasOne(u => u.UserLog)
-                .WithMany(u => u.Users)
-                .HasForeignKey(u => u.IdUserLog)
-                .HasConstraintName("FK_User_UserLog")
-                .OnDelete(DeleteBehavior.Restrict);
+        builder
+            .HasOne(u => u.UserLog)
+            .WithMany(u => u.Users)
+            .HasForeignKey(u => u.IdUserLog)
+            .HasConstraintName("FK_User_UserLog")
+            .OnDelete(DeleteBehavior.Restrict);
 
-            builder
-                .ToTable("User");
-        }
+        builder
+            .HasIndex(u => u.IdUser)
+            .HasDatabaseName("IX_User_ForCheckAuth")
+            .IncludeProperties(u => new
+            {
+                u.Email,
+                u.FirstName,
+                u.LastName,
+                u.PhoneNumber,
+                u.IdRole
+            });
+
+        builder
+            .ToTable("User");
     }
+}
