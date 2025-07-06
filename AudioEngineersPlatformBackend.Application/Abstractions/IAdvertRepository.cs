782be5371d31c01fe019999004a8f725738da897
Stanisław Stepka <stanislawstepka@Macbook-Pro-Stanisaw.local>
2025-07-06 21:04:47 +0200

    Edit and Delete advert authorization checks

--- AudioEngineersPlatformBackend.Application/Abstractions/IAdvertRepository.cs
+++ AudioEngineersPlatformBackend.Application/Abstractions/IAdvertRepository.cs
@@ -7,14 +7,17 @@ public interface IAdvertRepository
 {
     Task<Advert?> GetAdvertByIdUser(Guid idUser, CancellationToken cancellationToken);
     Task<Advert?> GetAdvertByIdAdvert(Guid idAdvert, CancellationToken cancellationToken);
-    Task<Advert> GetAdvertAndAdvertLogByIdAdvert(Guid idAdvert, CancellationToken cancellationToken);
+    Task<Advert?> GetAdvertAndAdvertLogByIdAdvert(Guid idAdvert, CancellationToken cancellationToken);
     Task<AdvertDetailsDto?> GetAdvertAssociatedDataByIdUser(Guid idUser, CancellationToken cancellationToken);
     Task<AdvertDetailsDto?> GetAdvertAssociatedDataByIdAdvert(Guid idAdvert, CancellationToken cancellationToken);
     Task<AdvertCategory?> GetAdvertCategoryByCategoryName(string categoryName, CancellationToken cancellationToken);
     Task<Advert> AddAdvert(Advert advert, CancellationToken cancellationToken);
     Task<AdvertLog> AddAdvertLog(AdvertLog advertLog, CancellationToken cancellationToken);
 
-    Task<PagedListDto<AdvertOverviewDto>> GetAllAdvertsSummariesWithPagination(string? sortOrder, int page,
+    Task<PagedListDto<AdvertOverviewDto>> GetAllAdvertsSummariesWithPagination(
+        string? sortOrder,
+        int page,
         int pageSize,
-        string? searchTerm, CancellationToken cancellationToken);
+        string? searchTerm, CancellationToken cancellationToken
+    );
 }
