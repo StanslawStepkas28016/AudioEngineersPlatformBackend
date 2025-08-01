bb4fa1224a1001c66676f7532d2b9a08444b205f
StanslawStepkas28016 <s28016@pjwstk.edu.pl>
2025-08-01 15:16:28 +0200

    Add strongly type FrontendSettings.cs configuration placeholder

--- AudioEngineersPlatformBackend.Application/DependencyInjection.cs
+++ AudioEngineersPlatformBackend.Application/DependencyInjection.cs
@@ -1,7 +1,9 @@
 ﻿using AudioEngineersPlatformBackend.Application.Abstractions;
 using AudioEngineersPlatformBackend.Application.Services;
 using AudioEngineersPlatformBackend.Application.Util.Cookies;
+using AudioEngineersPlatformBackend.Application.Util.CurrentUser;
 using AudioEngineersPlatformBackend.Application.Util.Tokens;
+using AudioEngineersPlatformBackend.Application.Util.UrlGenerator;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,13 +22,17 @@ public static class DependencyInjection
         // Add settings for JWT
         services.Configure<JwtSettings>(configuration.GetSection("JWTSettings"));
         services.AddScoped<ITokenUtil, TokenUtil>();
-        
+
         // Add HttpContextAccessor and CookieUtil for accessing cookies sent in the requests
         services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         services.AddScoped<ICookieUtil, CookieUtil>();
 
+        // Add frontend settings for generating an email reset Url
+        services.Configure<FrontendSettings>(configuration.GetSection("FrontendSettings"));
+        services.AddScoped<IUrlGeneratorUtil, UrlGeneratorUtil>();
+
         // Add services for current user
-        services.AddScoped<ICurrentUserService, CurrentUserService>();
+        services.AddScoped<ICurrentUserUtil, CurrentUserUtil>();
 
         return services;
     }
