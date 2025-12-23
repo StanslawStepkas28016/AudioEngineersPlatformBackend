d4f9e162582ecb705a0f05bb7d32ead1dabcd374
Stanisław Stepka <s28016@pjwstk.edu.pl>
2025-12-23 17:05:31 +0100

    Aspire compose

--- AppHost/AppHost.cs
+++ AppHost/AppHost.cs
@@ -2,16 +2,19 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+// Add docker-compose environment for publish.
+builder.AddDockerComposeEnvironment("docker-compose-artifacts");
+
 // Configure Postgres.
 var postgresUsername = builder
     .AddParameter("PostgresUsername");
 var postgresPassword = builder
     .AddParameter("PostgresPassword");
 
-var postgresDb = builder
+var db = builder
     .AddPostgres
     (
-        "audio-engineers-platform-db",
+        "soundbest-db",
         postgresUsername,
         postgresPassword,
         5432
@@ -24,10 +27,23 @@ var postgresDb = builder
         true
     );
 
+// Add all configurations.
+
 // Configure API.
+var api = builder
+    .AddProject<AudioEngineersPlatformBackend_API>("soundbest-api")
+    .WithEnvironment("LuckyPennySoftwareSettings__LicenseKey", "")
+    .WaitFor(db);
+
+// Configure client app.
 builder
-    .AddProject<AudioEngineersPlatformBackend_API>("AudioEngineersPlatformBackend")
-    .WaitFor(postgresDb);
+    .AddViteApp("soundbest-frontend", "../audio-engineers-platform-frontend", "prod")
+    .WithNpm()
+    .WithEnvironment("VITE_BACKEND_API", "http://localhost:5181/api")
+    .WithEnvironment("VITE_BACKEND_HUB", "http://localhost:5181/chat-hub")
+    .WithReference(api)
+    .WaitFor(api)
+    .WithEndpoint("http", endpoint => { endpoint.Port = builder.ExecutionContext.IsRunMode ? 5173 : null; });
 
 builder
     .Build()
